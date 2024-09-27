61daa1f9e6ddae1336131ce195b3e294b120d9e2
Liam Hammett <liamhammett@gmail.com>
2024-09-27 18:10:25 +0100

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,14 +63,10 @@
                                     <svg class="absolute text-indigo-500 size-6 left-1 top-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                                         <path fill-rule="evenodd" d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" clip-rule="evenodd" />
                                         <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
-                                        </svg>
+                                    </svg>
                                     Just run it.
                                 </dt>
-                                <dd class="inline">Just run <x-code>cpx vendor/package</x-code> to invoke
-                                    the command from a package. If a package has more than one command, pass through
-                                    the command name like <x-code>cpx vendor/package &lt;command&gt;</x-code>.
-                                    If you need to pass parameters to the command, just append them on the end. It's
-                                    just intuitive.</dd>
+                                <dd class="inline">Just run <x-code>cpx vendor/package</x-code> to invoke the command from a package. If a package has more than one command, pass through the command name like <x-code>cpx vendor/package &lt;command&gt;</x-code><br>If you need to pass further arguments or options to the command, append them on the end. It's just intuitive.</dd>
                             </div>
                             <div class="relative pl-9">
                                 <dt class="inline font-semibold text-white">
